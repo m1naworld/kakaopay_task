5959e1efac11d854073b81829663d4e5fa4608e6
mina <m1nna@naver.com>
2023-03-04 05:14:50 +0900

    init

---
+++ src/main/java/com/example/kakaopay/CoffeeApplication.java
@@ -0,0 +1,15 @@
+package com.example.kakaopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeApplication.class, args);
+	}
+
+}
+
+
