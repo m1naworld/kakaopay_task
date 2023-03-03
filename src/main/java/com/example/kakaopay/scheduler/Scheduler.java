2bdf439c3213b22c7fc5c2ac6569ff678cd80983
mina <m1nna@naver.com>
2023-03-04 05:48:34 +0900

    [feat] SchedulerLock 설정
    다중 서버일때도 스케쥴러가 한번만 실행될 수 있도록 SchedulerLock 설정

--- src/main/java/com/example/kakaopay/scheduler/Scheduler.java
+++ src/main/java/com/example/kakaopay/scheduler/Scheduler.java
@@ -4,8 +4,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import com.example.kakaopay.menu.entity.BestMenu;
 import com.example.kakaopay.menu.repository.BestMenuRepository;
@@ -19,6 +26,14 @@ public class Scheduler {
 
 	private final BestMenuRepository bestMenuRepository;
 
+	@Bean
+	public LockProvider lockProvider(DataSource dataSource) {
+		return new JdbcTemplateLockProvider(dataSource);
+	}
+
+	private static final String SEC_30 = "PT30S";
+
+	@SchedulerLock(name = "runScenarioOneTime", lockAtMostFor = SEC_30, lockAtLeastFor = SEC_30)
 	@Scheduled(cron = "0 0 0 * * *")
 	public void bestMenuSch() {
 
