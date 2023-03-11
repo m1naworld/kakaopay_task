9bb790c6228912dede3d4954277013ae05e82a76
mina <m1nna@naver.com>
2023-03-11 22:17:46 +0900

    [docs] 스케줄링 lock 테이블 ddl 작성

--- src/main/java/com/example/kakaopay/sql/mysql-ddl.sql
+++ src/main/java/com/example/kakaopay/sql/mysql-ddl.sql
@@ -36,3 +36,12 @@ CREATE TABLE best_menu (
     price INT NOT NULL,
     order_count INT NOT NULL
 );
+
+CREATE TABLE shedlock (
+    name varchar(64) NOT NULL COMMENT '스케줄잠금이름',
+    lock_until timestamp(3) NULL DEFAULT NULL COMMENT '잠금기간',
+    locked_at timestamp(3) NULL DEFAULT NULL COMMENT '잠금일시',
+    locked_by varchar(255) DEFAULT NULL COMMENT '잠금신청자',
+    PRIMARY KEY (name)
+);
+
